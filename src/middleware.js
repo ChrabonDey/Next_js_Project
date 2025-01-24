455af5b0cdf9bfcd064306c79cdeb2edc9088a47
Chrabon Dey Sarker <chrabondey@gmail.com>
2025-01-24 22:29:49 +0600

    here i commit my git

--- src/middleware.js
+++ src/middleware.js
@@ -1,31 +1,23 @@
 import { getKindeServerSession } from "@kinde-oss/kinde-auth-nextjs/server";
-import { NextRequest, NextResponse } from "next/server";
+import { NextResponse } from "next/server";
 
 export async function middleware(request) {
-  
   const { getUser } = getKindeServerSession();
   const user = await getUser();
-  
- 
-  const url = new URL(request.url);
-  const postId = url.pathname.match(/^\/post\/post-details\/([^/]+)/)?.[1];  
 
- 
   if (!user) {
-    return NextResponse.redirect(
-      new URL(`/api/auth/login?post_login_redirect_url=/post/post-details/${postId}`, request.url)
-    );
-  }
-  if (!user) {
-    return NextResponse.redirect(
-      new URL("/api/auth/login?post_login_redirect_url=/profile", request.url)
-    );
-  }
+    const url = new URL(request.url);
+    const redirectPath = url.pathname.includes("/post/post-details/")
+      ? `/api/auth/login?post_login_redirect_url=${url.pathname}`
+      : "/api/auth/login?post_login_redirect_url=/profile";
 
+    return NextResponse.redirect(new URL(redirectPath, request.url));
+  }
 
   return NextResponse.next();
 }
 
+
 export const config = {
-  matcher: ['/post/post-details/:post_id','/profile'],  
+  matcher: ["/post/post-details/:path*", "/profile"], 
 };
